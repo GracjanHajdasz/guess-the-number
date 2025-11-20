47ac7858e5c98027ce5237276059a52179cb357e
Gracjan Hajdasz <b3nny3@wp.pl>
2025-11-20 17:57:38 +0100

    chyba zrobione juz pozdro

--- main.cpp
+++ main.cpp
@@ -3,14 +3,55 @@
 #include <ctime>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 bool guessed = false;
 int numberToGuess;
+bool playing = false;
 
-void scoreboard()
+// scoreboard
+string username;
+string level;
+string userTrials;
+vector<string> usernames;
+vector<string> levels;
+vector<int> trials;
+
+void sortScoreboard()
 {
+    for (int i = 0; i < trials.size() - 1; i++)
+    {
+        for (int j = 0; j < trials.size() - i - 1; j++)
+        {
+            if (trials[j] > trials[j + 1])
+            {
+                swap(trials[j], trials[j + 1]);
+                swap(usernames[j], usernames[j + 1]);
+                swap(levels[j], levels[j + 1]);
+            }
+        }
+    }
+    reverse(trials.begin(), trials.end());
+    reverse(usernames.begin(), usernames.end());
+    reverse(levels.begin(), levels.end());
+    // top 5
+    if (trials.size() > 5)
+    {
+        trials.resize(5);
+        usernames.resize(5);
+        levels.resize(5);
+    }
+}
+
+void viewScoreboard()
+{
+    cout << "tabalica wynikow:" << endl;
+    for (int i = 0; i < usernames.size(); i++)
+    {
+        cout << usernames[i] << " | " << levels[i] << " | " << trials[i] << endl;
+    }
 }
 
 void welcome()
@@ -27,19 +68,33 @@ void welcome()
         if (action == "latwy")
         {
             numberToGuess = rand() % 51;
+            level = "latwy";
+            playing = true;
         }
         else if (action == "sredni")
         {
             numberToGuess = rand() % 101;
+            level = "sredni";
+            playing = true;
         }
         else if (action == "trudny")
         {
             numberToGuess = rand() % 201;
+            level = "trudny";
+            playing = true;
         }
         else
         {
             cout << "nieprawidlowy poziom trudnosci" << endl;
         }
+    }
+    else if (action == "b")
+    {
+        viewScoreboard();
+    }
+    else
+    {
+        cout << "nieprawidlowa akcja" << endl;
     }
 }
 
@@ -50,7 +105,6 @@ void gameplay()
     {
 
         int userGuess;
-        string username;
 
         cout << "proba " << i << " podaj liczbe: ";
         cin >> userGuess;
@@ -71,6 +125,12 @@ void gameplay()
         }
         i++;
     }
+    playing = false;
+    i--;
+
+    usernames.push_back(username);
+    levels.push_back(level);
+    trials.push_back(i);
 }
 
 int main()
@@ -78,8 +138,11 @@ int main()
 
     while (true)
     {
-        welcome();
-        gameplay();
+
+        if (!playing)
+            welcome();
+        else
+            gameplay();
         guessed = false;
     }
 
